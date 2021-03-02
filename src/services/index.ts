6920252b90fc7c225cc130d3872a918df2ec9004
Jayesh <jayesh.anandani@aravsys.com>
2021-03-02 12:52:40 +0530

    FIRST commit with feather auth+todo

---
+++ src/services/index.ts
@@ -0,0 +1,9 @@
+import { Application } from '../declarations';
+import users from './users/users.service';
+import todo from './todo/todo.service';
+// Don't remove this comment. It's needed to format import lines nicely.
+
+export default function (app: Application): void {
+  app.configure(users);
+  app.configure(todo);
+}
